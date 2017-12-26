62b00a2a26ce5631b968c157fd42abd65227f78d
solufemi91 <stevenoo@spartaglobal.com>
2017-12-26 19:51:07 +0000

    added the fourth service

---
+++ lib/services/get_neo_today.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require 'json'
+
+class GetNeoToday
+  include HTTParty
+
+  base_uri 'https://api.nasa.gov/neo'
+
+  def initialize
+    @apiKey = "jGx3T69k38ueDfGl1kK2i1haHghETqwSfJwGShyr"
+  end
+
+  def get_neo_today
+    @a_specific_neo =JSON.parse(self.class.get("/rest/v1/feed/today?detailed=true&api_key=#{@apiKey}").body)
+  end
+
+end
+
+x = GetNeoToday.new
+
+puts x.get_neo_today
