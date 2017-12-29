743cf4fe4af2a17b63bd9bac87f11606dc5586e3
solufemi91 <stevenoo@spartaglobal.com>
2017-12-29 18:02:44 +0000

    added spec helper for browse

--- lib/nasa_api.rb
+++ lib/nasa_api.rb
@@ -1,9 +1,14 @@
 require_relative 'services/get_lookup_service'
+require_relative 'services/get_browse_service'
 
 class Nasa_api
 
   def lookup_service
     GetLookup.new
+  end
+
+  def browse_service
+    GetListOfNeo.new
   end
 
 end
---
+++ spec/get_browse_service_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Nasa_api do
+  context 'For the get_browse service' do
+
+    before(:all) do
+      @browse = Nasa_api.new.browse_service
+      @results = @browse.get_list_of_neo
+    end
+
+    it "I expect the results to be a hash" do
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end
