a9345ffad791d47f5b231ff6c23b07655c73cc1d
solufemi91 <stevenoo@spartaglobal.com>
2017-12-30 15:10:59 +0000

    added services for feed service

--- lib/services/get_feed_service.rb
+++ lib/services/get_feed_service.rb
@@ -11,10 +11,77 @@ class GetSpecifiedRange
   end
 
   def get_specified_range(start_date,end_date)
-    @a_list_of_neo =JSON.parse(self.class.get("/rest/v1/feed?start_date=#{start_date}&end_date=#{end_date}&api_key=#{@apiKey}").body)
+    @results =JSON.parse(self.class.get("/rest/v1/feed?start_date=#{start_date}&end_date=#{end_date}&api_key=#{@apiKey}").body)
   end
+
+  def get_main_links
+   @results['links']
+  end
+
+  def get_next
+    get_main_links['next']
+  end
+
+  def get_previous
+    get_main_links['next']
+  end
+
+  def get_self
+    get_main_links['self']
+  end
+
+  def get_element_count
+   @results['element_count']
+  end
+
+  def get_near_earth_objects
+    @results['near_earth_objects']
+  end
+
+
+  def get_links
+    @my_array = []
+    @neo = get_near_earth_objects
+    keys = @neo.keys
+    keys.each do |date|
+      @neo[date].each do |objects|
+        @my_array.push(objects['links'])
+      end
+    end
+    @my_array
+  end
+
+  def get_links_self_key_value
+    @my_array = []
+    @neo = get_near_earth_objects
+    keys = @neo.keys
+    keys.each do |date|
+      @neo[date].each do |objects|
+        @my_array.push(objects['links']['self'])
+      end
+    end
+    @my_array
+  end
+
+  def get_neo_reference_id_key_value
+    @my_array = []
+    @neo = get_near_earth_objects
+    keys = @neo.keys
+    keys.each do |date|
+      @neo[date].each do |objects|
+        @my_array.push(objects['neo_reference_id'])
+      end
+    end
+    @my_array
+  end
+
+
+
+
+
 
 end
 
 x = GetSpecifiedRange.new
-puts x.get_specified_range('2017-12-19','2017-12-25')
+x.get_specified_range('2015-09-08','2015-09-07')
+puts x.get_neo_reference_id_key_value
